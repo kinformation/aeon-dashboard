f911aec80cf09fbf328123f018f0ef0b34b57855
Your Name <you@example.com>
2022-12-29 23:37:01 +0900

    Commit made from VS Code Azure Static Web Apps

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import azure from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: azure()
 	}
 };
 
