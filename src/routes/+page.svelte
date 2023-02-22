36de1b525551d4ef3e1cd128d99e9f4878f646ef
Your Name <you@example.com>
2023-02-22 23:36:08 +0900

    modified:   package-lock.json modified:   package.json modified:   src/routes/+page.svelte modified:   src/stores.js new file:   src/supabase.js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,41 @@
 <script>
+  import { onMount } from 'svelte'
+  import { storesRow, stores } from '../stores.js'
+  import supabase from '../supabase.js'
+
   import MyTable from '$lib/MyTable.svelte'
   import MyMap from '$lib/MyMap.svelte'
   import MyChart from '$lib/MyChart.svelte'
   import MyChart2 from '$lib/MyChart2.svelte'
+
+  onMount(async () => {
+    // supabase DB より店舗情報取得
+    storesRow.set(await supabase.stores.get())
+  })
 </script>
 
-<main class="p-4">
-  <div class="grid grid-cols-4 gap-4">
-    <div class="grid col-span-4">
-      <div class="table-area">
-        <MyTable />
+{#if $stores.length > 0}
+  <main class="p-4">
+    <div class="grid grid-cols-4 gap-4">
+      <div class="grid col-span-4">
+        <div class="table-area">
+          <MyTable />
+        </div>
+      </div>
+      <div class="grid col-span-4 lg:col-span-2">
+        <div class="map-area">
+          <MyMap />
+        </div>
+      </div>
+      <div class="grid col-span-2 lg:col-span-1">
+        <MyChart />
+      </div>
+      <div class="grid col-span-2 lg:col-span-1">
+        <MyChart2 />
       </div>
     </div>
-    <div class="grid col-span-4 lg:col-span-2">
-      <div class="map-area">
-        <MyMap />
-      </div>
-    </div>
-    <div class="grid col-span-2 lg:col-span-1">
-      <MyChart />
-    </div>
-    <div class="grid col-span-2 lg:col-span-1">
-      <MyChart2 />
-    </div>
-  </div>
-</main>
+  </main>
+{/if}
 
 <style>
 </style>
---
+++ src/supabase.js
@@ -0,0 +1,15 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default {
+  stores: {
+    async get() {
+      const { data } = await supabase.from('stores').select('*')
+      return data
+    },
+  },
+}
