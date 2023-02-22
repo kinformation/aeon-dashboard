7af733b7672d55886a2b524b3f8a0ef52ef743c8
Your Name <you@example.com>
2023-02-22 23:51:34 +0900

    modified:   src/supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,15 +1,28 @@
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+// const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
-const supabase = createClient(supabaseUrl, supabaseKey)
+// const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default {
   stores: {
     async get() {
-      const { data } = await supabase.from('stores').select('*')
-      return data
+      // const { data } = await supabase.from('stores').select('*')
+      // return data
+      return [
+        {
+          id: 2,
+          region: '北海道地方',
+          pref: '北海道',
+          brand: 'イオン',
+          store_name: 'イオン札幌桑園店',
+          company: 'イオン北海道株式会社',
+          address: '北海道札幌市中央区北8条西14-28',
+          lng: 141.33367451,
+          lat: 43.06852979,
+        },
+      ]
     },
   },
 }
